328c074529410cfea04395124218d042154e5644
Dan Ko <danko56666@gmail.com>
2021-08-24 03:40:07 -0400

    setup env

--- src/api/axiosConfig.ts
+++ src/api/axiosConfig.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
+import { API_URL } from "@env";
 
 const instance = axios.create({
-  baseURL: `https://www.googleapis.com/`,
+  baseURL: `${API_URL}`,
 });
 
 export default instance;
