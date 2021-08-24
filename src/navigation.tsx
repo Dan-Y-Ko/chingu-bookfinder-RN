2223de3bce79797225689d263638fe295a30e52a
Dan Ko <danko56666@gmail.com>
2021-08-23 20:17:03 -0400

    setup root navigation, stack navigator for books list, and basic screen for book list

---
+++ src/navigation.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+import BookListScreen from "./screens/BookListScreen";
+
+export type BookListStackParamList = {
+  Home: undefined;
+};
+
+// create container navigation in case app needs authentication or onboarding before proceeding to main app
+const RootStack = (): JSX.Element => (
+  <NavigationContainer>
+    <BookListNavigator />
+  </NavigationContainer>
+);
+
+// const forFade = ({ current }) => ({
+//   cardStyle: {
+//     opacity: current.progress,
+//   },
+// });
+
+const BookListStack = createNativeStackNavigator<BookListStackParamList>();
+
+const BookListNavigator = (): JSX.Element => (
+  <BookListStack.Navigator initialRouteName="Home">
+    <BookListStack.Screen
+      name="Home"
+      component={BookListScreen}
+      // options={{ gestureEnabled: false, cardStyleInterpolator: forFade }}
+    />
+    {/* <BookListStack.Screen
+      name="BookDetail"
+      component={BookDetailScreen}
+      options={{ gestureEnabled: false, cardStyleInterpolator: forFade }}
+    /> */}
+  </BookListStack.Navigator>
+);
+
+export default RootStack;
---
+++ src/screens/BookListScreen.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { View } from "react-native";
+import { Text } from "react-native-elements";
+
+const BookListScreen = () => (
+  <View>
+    <Text h1>Hello</Text>
+  </View>
+);
+
+export default BookListScreen;
